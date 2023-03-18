e800d00e57ecfb689b7d8c2a2422557a2275450d
Serhii <serhii.mask@gmail.com>
2023-03-18 11:55:38 +0200

    feat: create enemies

--- main.py
+++ main.py
@@ -1,42 +1,76 @@
-import pygame, random
+import pygame
+import random
 from pygame. constants import QUIT, K_DOWN, K_UP, K_LEFT, K_RIGHT
 
 pygame.init()
+
+FPS = pygame.time.Clock()
 
 screen = width, heigth = 800, 600
 
 BLACK = 0, 0, 0
 WHITE = 255, 255, 255
+RED = 255, 0, 0
 
 main_surface = pygame.display.set_mode(screen)
 
 ball = pygame.Surface((15, 15))
 ball.fill(WHITE)
-bal_rect = ball.get_rect()
-ball_speed = 1
+ball_rect = ball.get_rect()
+ball_speed = 5
+
+
+def create_enemy():
+    enemy = pygame.Surface((20, 20))
+    enemy.fill(RED)
+    enemy_rect = pygame.Rect(
+        width, random.randint(0, heigth), *enemy.get_size())
+    enemy_speed = random.randint(2, 5)
+    return [enemy, enemy_rect, enemy_speed]
+
+
+CREATE_ENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(CREATE_ENEMY, 1500)
+
+enemies = []
 
 is_working = True
 
 while is_working:
-   for event in pygame.event.get():
-      if event.type == QUIT:
-         is_working = False  
-   
-   pressed_keys = pygame.key.get_pressed()
+    FPS.tick(60)
 
-   main_surface.fill(BLACK)
-   
-   main_surface.blit(ball, bal_rect)
-   if pressed_keys[K_DOWN]:
-      bal_rect = bal_rect.move(0, ball_speed)
-      
-   if pressed_keys[K_UP]:
-      bal_rect = bal_rect.move(0, -ball_speed)
-      
-   if pressed_keys[K_LEFT]:
-      bal_rect = bal_rect.move(-ball_speed, 0)
-      
-   if pressed_keys[K_RIGHT]:
-      bal_rect = bal_rect.move(ball_speed, 0)
-          
-   pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            is_working = False
+
+        if event.type == CREATE_ENEMY:
+            enemies.append(create_enemy())
+
+    pressed_keys = pygame.key.get_pressed()
+
+    main_surface.fill(BLACK)
+
+    main_surface.blit(ball, ball_rect)
+    for enemy in enemies:
+        enemy[1] = enemy[1].move(-enemy[2], 0)
+        main_surface.blit(enemy[0], enemy[1])
+
+        if enemy[1].left < 0:
+            enemies.pop(enemies.index(enemy))
+
+        if ball_rect.colliderect(enemy[1]):
+            enemies.pop(enemies.index(enemy))
+
+    if pressed_keys[K_DOWN]:
+        ball_rect = ball_rect.move(0, ball_speed)
+
+    if pressed_keys[K_UP]:
+        ball_rect = ball_rect.move(0, -ball_speed)
+
+    if pressed_keys[K_LEFT]:
+        ball_rect = ball_rect.move(-ball_speed, 0)
+
+    if pressed_keys[K_RIGHT]:
+        ball_rect = ball_rect.move(ball_speed, 0)
+
+    pygame.display.flip()

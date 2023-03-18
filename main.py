6d0e340a9e55bd38da6040dfb04dbef93a0cf654
Serhii <serhii.mask@gmail.com>
2023-03-18 11:01:01 +0200

    feat: management ball

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame, random
-from pygame. constants import QUIT
+from pygame. constants import QUIT, K_DOWN, K_UP, K_LEFT, K_RIGHT
 
 pygame.init()
 
@@ -13,26 +13,30 @@ main_surface = pygame.display.set_mode(screen)
 ball = pygame.Surface((15, 15))
 ball.fill(WHITE)
 bal_rect = ball.get_rect()
-ball_speed = [1, 1]
+ball_speed = 1
 
 is_working = True
 
 while is_working:
    for event in pygame.event.get():
       if event.type == QUIT:
-         is_working = False 
-         
-   bal_rect = bal_rect.move(ball_speed)
+         is_working = False  
    
-   if bal_rect.bottom >= heigth or bal_rect.top <= 0:
-      ball_speed[1] = -ball_speed[1]
-      ball.fill(random.sample(range(255), 3))
-   if bal_rect.right >= width or bal_rect.left <= 0:
-      ball_speed[0] = -ball_speed[0]
-      ball.fill(random.sample(range(255), 3))
+   pressed_keys = pygame.key.get_pressed()
 
    main_surface.fill(BLACK)
    
    main_surface.blit(ball, bal_rect)
+   if pressed_keys[K_DOWN]:
+      bal_rect = bal_rect.move(0, ball_speed)
+      
+   if pressed_keys[K_UP]:
+      bal_rect = bal_rect.move(0, -ball_speed)
+      
+   if pressed_keys[K_LEFT]:
+      bal_rect = bal_rect.move(-ball_speed, 0)
+      
+   if pressed_keys[K_RIGHT]:
+      bal_rect = bal_rect.move(ball_speed, 0)
           
    pygame.display.flip()

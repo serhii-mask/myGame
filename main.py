18c7631e62cbffc972b2e2c9aebd7e8f8a3374bb
Serhii <serhii.mask@gmail.com>
2023-03-19 11:59:36 +0200

    feat: add photos

--- main.py
+++ main.py
@@ -17,15 +17,19 @@ font = pygame.font.SysFont('Verdana', 20)
 
 main_surface = pygame.display.set_mode(screen)
 
-ball = pygame.Surface((20, 20))
-ball.fill(WHITE)
-ball_rect = ball.get_rect()
-ball_speed = 5
+# player = pygame.Surface((20, 20))
+# player.fill(WHITE)
+player = pygame.transform.scale(pygame.image.load(
+    'player.png').convert_alpha(), (91, 38))
+player_rect = player.get_rect()
+player_speed = 10
 
 
 def create_enemy():
-    enemy = pygame.Surface((20, 20))
-    enemy.fill(RED)
+    # enemy = pygame.Surface((20, 20))
+    # enemy.fill(RED)
+    enemy = pygame.transform.scale(
+        pygame.image.load('enemy.png').convert_alpha(), (68, 24))
     enemy_rect = pygame.Rect(
         width, random.randint(0, heigth), *enemy.get_size())
     enemy_speed = random.randint(2, 5)
@@ -33,12 +37,18 @@ def create_enemy():
 
 
 def create_bonus():
-    bonus = pygame.Surface((20, 20))
-    bonus.fill(GREEN)
+    # bonus = pygame.Surface((20, 20))
+    # bonus.fill(GREEN)
+    bonus = pygame.transform.scale(
+        pygame.image.load('bonus.png').convert_alpha(), (90, 149))
     bonus_rect = pygame.Rect(
         random.randint(0, width), 0, *bonus.get_size())
     bonus_speed = random.randint(4, 6)
     return [bonus, bonus_rect, bonus_speed]
+
+
+bg = pygame.transform.scale(pygame.image.load(
+    'background.png').convert(), screen)
 
 
 CREATE_ENEMY = pygame.USEREVENT + 1
@@ -69,9 +79,10 @@ while is_working:
 
     pressed_keys = pygame.key.get_pressed()
 
-    main_surface.fill(BLACK)
+    main_surface.fill(WHITE)
+    main_surface.blit(bg, (0, 0))
 
-    main_surface.blit(ball, ball_rect)
+    main_surface.blit(player, player_rect)
 
     main_surface.blit(font.render(str(scores), True, WHITE), (width - 30, 0))
 
@@ -79,33 +90,33 @@ while is_working:
         enemy[1] = enemy[1].move(-enemy[2], 0)
         main_surface.blit(enemy[0], enemy[1])
 
-        if enemy[1].left < 0:
+        if enemy[1].left < -68:
             enemies.pop(enemies.index(enemy))
 
-        if ball_rect.colliderect(enemy[1]):
+        if player_rect.colliderect(enemy[1]):
             is_working = False
 
     for bonus in bonuses:
         bonus[1] = bonus[1].move(0, bonus[2])
         main_surface.blit(bonus[0], bonus[1])
 
-        if bonus[1].bottom >= heigth:
+        if bonus[1].bottom >= heigth + 149:
             bonuses.pop(bonuses.index(bonus))
 
-        if ball_rect.colliderect(bonus[1]):
+        if player_rect.colliderect(bonus[1]):
             bonuses.pop(bonuses.index(bonus))
             scores += 1
 
-    if pressed_keys[K_DOWN] and not ball_rect.bottom >= heigth:
-        ball_rect = ball_rect.move(0, ball_speed)
+    if pressed_keys[K_DOWN] and not player_rect.bottom >= heigth:
+        player_rect = player_rect.move(0, player_speed)
 
-    if pressed_keys[K_UP] and not ball_rect.top <= 0:
-        ball_rect = ball_rect.move(0, -ball_speed)
+    if pressed_keys[K_UP] and not player_rect.top <= 0:
+        player_rect = player_rect.move(0, -player_speed)
 
-    if pressed_keys[K_LEFT] and not ball_rect.left <= 0:
-        ball_rect = ball_rect.move(-ball_speed, 0)
+    if pressed_keys[K_LEFT] and not player_rect.left <= 0:
+        player_rect = player_rect.move(-player_speed, 0)
 
-    if pressed_keys[K_RIGHT] and not ball_rect.right >= width:
-        ball_rect = ball_rect.move(ball_speed, 0)
+    if pressed_keys[K_RIGHT] and not player_rect.right >= width:
+        player_rect = player_rect.move(player_speed, 0)
 
     pygame.display.flip()
